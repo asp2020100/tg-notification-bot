cd677e1ae7ab4ac7d09bbd701ec304c6de4cc40e
R.M.Y.P.Rathnayake <122531660+asp2020100@users.noreply.github.com>
2023-04-09 14:58:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = '5852515114:AAGHtynJ8WMiL4BCZeEHWe7SujwBKH6S-sU'
+
+MONGO_DB_URL = 'mongodb+srv://batrow:10300@brodcast.mxelgo5.mongodb.net/?retryWrites=true&w=majority'
+
+GROUP_ID = -1001754762425
+
+ADMIN_ID = 5278296386
+
